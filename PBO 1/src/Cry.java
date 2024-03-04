03e91d0ab63b174b20a5b4ada41c2111cb6e3c62
khoirunnisaah18 <khoirunnisa18@wemail.umm.ac.id>
2024-03-04 14:10:53 +0700

    first commit

---
+++ PBO 1/src/Cry.java
@@ -0,0 +1,13 @@
+public class Cry {
+    public String name = "Cat";
+    public String genre = "Laki laki";
+    public int age = 2;
+    public String colour = "grey";
+
+    public void Cry(String name, int age, String genre, String colour){
+        System.out.println("Nama Cry  : "+name);
+        System.out.println("Jenis Cry : "+genre);
+        System.out.println("Umur Cry  :  "+age);
+        System.out.println("Warna Cry : "+colour);
+    }
+}
---
+++ PBO 1/src/Me.java
@@ -0,0 +1,6 @@
+public class Me {
+    public static void main(String[] args){
+        Cry hewanku = new Cry();
+        hewanku.Cry("Cat",2 ,"Laki laki", "grey");
+    }
+}
